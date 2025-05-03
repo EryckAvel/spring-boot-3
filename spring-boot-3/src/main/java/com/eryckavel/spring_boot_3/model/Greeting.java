e79b0c1b12b8a8ec1bd3cc460e2ec265934972cf
Eryck Avel <eryckavel@gmail.com>
2025-05-03 12:00:09 -0300

    Initial Commit Project

---
+++ spring-boot-3/src/main/java/com/eryckavel/spring_boot_3/model/Greeting.java
@@ -0,0 +1,4 @@
+package com.eryckavel.spring_boot_3.model;
+
+public record Greeting(Long id, String content) {
+}
---
+++ spring-boot-3/src/test/java/com/eryckavel/spring_boot_3/StartupTests.java
@@ -0,0 +1,13 @@
+package com.eryckavel.spring_boot_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

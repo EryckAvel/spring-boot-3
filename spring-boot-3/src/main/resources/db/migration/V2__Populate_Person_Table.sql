1aba9433d370af9f3826cbcd506303df9d5fe141
Eryck Avel <eryckavel@gmail.com>
2025-06-19 11:12:49 -0300

    S11 - Aplicando migrations

---
+++ spring-boot-3/src/main/resources/db/migration/V2__Populate_Person_Table.sql
@@ -0,0 +1,15 @@
+INSERT INTO springboot3.person
+(id, adress, first_name, gender, last_name)
+VALUES(1, 'Rua 27, 1206', 'Eryck', 'Masculino', 'Calisto Avelino');
+INSERT INTO springboot3.person
+(id, adress, first_name, gender, last_name)
+VALUES(4, 'Rua 27, 1206', 'Maria Eduarda', 'Feminino', 'de Almeida Bardosa');
+INSERT INTO springboot3.person
+(id, adress, first_name, gender, last_name)
+VALUES(5, 'Rua 27, 1206', 'Maria Eduarda', 'Feminino', 'de Almeida Bardosa');
+INSERT INTO springboot3.person
+(id, adress, first_name, gender, last_name)
+VALUES(6, 'Rua 27, 1206', 'Maria Eduarda', 'Feminino', 'de Almeida Bardosa');
+INSERT INTO springboot3.person
+(id, adress, first_name, gender, last_name)
+VALUES(7, 'Rua 27, 1206', 'Maria Eduarda', 'Feminino', 'de Almeida Bardosa');

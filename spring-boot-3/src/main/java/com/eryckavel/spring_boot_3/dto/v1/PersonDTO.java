a4e0df756b43ce771f052910ea69b40a71863e8e
Eryck Avel <eryckavel@gmail.com>
2025-06-19 11:52:38 -0300

    S12 - Json Serialization

--- spring-boot-3/src/main/java/com/eryckavel/spring_boot_3/dto/v1/PersonDTO.java
+++ spring-boot-3/src/main/java/com/eryckavel/spring_boot_3/dto/v1/PersonDTO.java
@@ -1,15 +1,31 @@
 package com.eryckavel.spring_boot_3.dto.v1;
 
+import com.eryckavel.spring_boot_3.serializer.GenderSerializer;
+import com.fasterxml.jackson.annotation.*;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
 import java.io.Serializable;
+import java.util.Date;
 import java.util.Objects;
 
+//@JsonPropertyOrder({"id" , "firstName", "adress", "lastName", "gender"})
+//@JsonFilter("PersonFilter")
 public class PersonDTO implements Serializable {
 
     private Long id;
+    //@JsonProperty("first_name")
     private String firstName;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private String lastName;
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
+//    private String phoneNumber;
     private String adress;
+//    @JsonFormat(pattern = "dd/MM/yyyy")
+//    private Date birthDay;
+    //@JsonIgnore
+    @JsonSerialize(using = GenderSerializer.class)
     private String gender;
+//    private String sensitiveData;
 
     public PersonDTO() {
     }
@@ -66,8 +82,8 @@ public class PersonDTO implements Serializable {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        PersonDTO person = (PersonDTO) o;
-        return Objects.equals(id, person.id) && Objects.equals(firstName, person.firstName) && Objects.equals(lastName, person.lastName) && Objects.equals(adress, person.adress) && Objects.equals(gender, person.gender);
+        PersonDTO personDTO = (PersonDTO) o;
+        return Objects.equals(id, personDTO.id) && Objects.equals(firstName, personDTO.firstName) && Objects.equals(lastName, personDTO.lastName) && Objects.equals(adress, personDTO.adress) && Objects.equals(gender, personDTO.gender);
     }
 
     @Override
